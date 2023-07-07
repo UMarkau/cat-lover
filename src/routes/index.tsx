b32ca230e6e0198ebdbabdc0b2cb5804be07aa53
Uladzimir <kitanokiller@gmail.com>
2023-07-07 12:40:01 +0200

    Implement nav menu.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 import { MainPage } from "../components/MainPage";
+import { NavMenu } from "../components/NavMenu";
 
 export const Router = () => {
   return (
     <BrowserRouter>
+      <NavMenu />
       <Routes>
         {["/", "/cat/:catId"].map((path) => (
           <Route key={path} path={path} element={<MainPage />} />
